ac1753015fd44459cfa7c7e389befe9a60f25630
JULIUS SUTRISNO <julius3788@gmail.com>
2025-10-20 16:34:01 +0700

    fix: dynamic base path for both Vercel and GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [react(),
-   tailwindcss(),
-  ],
-  base: '/quiz-mitos-fakta/',
 
+export default defineConfig({
+  plugins: [
+    react(),
+    tailwindcss(),
+  ],
+  base: process.env.VERCEL ? '/' : '/quiz-mitos-fakta/',
 })
